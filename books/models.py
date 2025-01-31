9846be00c376cd9a0a6c7600a281d970ae732b4f
woho4u <okebedrift@gmail.com>
2025-01-31 11:02:57 +0100

    create book model

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.CharField(default='This is a great book!', max_length=255)
+    genre = models.CharField(max_length=100)
+    keywords = models.JSONField(default=list, help_text='A list of keywords')
+    coverImage = models.URLField(null=True, help_text='Cover image link')
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
