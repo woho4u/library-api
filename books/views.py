f9d234b18fb1e14dfb4f2da46e077c063be74252
Oliver Eriksen <okebedrift@gmail.com>
2025-02-01 11:52:32 +0100

    add SearchFilter for books

--- books/views.py
+++ books/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.filters import SearchFilter
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'author']
