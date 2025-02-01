fb443eab419d853deaccafdef7496560b58ec851
Oliver Eriksen <okebedrift@gmail.com>
2025-02-01 11:51:47 +0100

    change maxlength for description

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    description = models.CharField(default='This is a great book!', max_length=255)
+    description = models.CharField(default='This is a great book!', max_length=400)
     genre = models.CharField(max_length=100)
     keywords = models.JSONField(default=list, help_text='A list of keywords')
     coverImage = models.URLField(null=True, help_text='Cover image link')
