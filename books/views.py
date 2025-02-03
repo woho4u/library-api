2511f1a48abe3acedc90ea0485613078f7af0efa
Oliver Eriksen <okebedrift@gmail.com>
2025-02-03 22:04:05 +0100

    Implement django_filters for multi param search

--- books/views.py
+++ books/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import BookFilter
 from rest_framework.filters import SearchFilter
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['title', 'author', 'id']
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']  # keep search for text if desired
