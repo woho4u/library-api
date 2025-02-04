71132208e97881441b38adb6f3c4226791dbc96a
Oliver Eriksen <okebedrift@gmail.com>
2025-02-04 21:47:47 +0100

    Create view for user

---
+++ users/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.filters import SearchFilter
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['username']  # keep search for text if desired
