ac36f7c44ca84b02e4d0e6dbb46666c584740b88
Oliver Eriksen <okebedrift@gmail.com>
2025-02-04 21:49:36 +0100

    add user api route

--- library_backend/urls.py
+++ library_backend/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from books.views import BookViewSet
+from users.views import UserViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
