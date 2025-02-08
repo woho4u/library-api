50d16c9fa975c71e5e09477c98694a3ff9df3778
Oliver Eriksen <okebedrift@gmail.com>
2025-02-08 23:19:21 +0100

    Create serializers for readingLists and readingListBook

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,42 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, ReadingList, ReadingListBook, Book
+from books.serializers import BookSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class ReadingListBookSerializer(serializers.ModelSerializer):
+
+    book = BookSerializer(read_only=True)
+
+    book_id = serializers.PrimaryKeyRelatedField(
+        queryset=Book.objects.all(),
+        source='book',
+        write_only=True
+    )
+    class Meta:
+        model = ReadingListBook
+        fields = '__all__'
+
+
+class ReadingListSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        source='user',
+        write_only=True
+    )
+
+    reading_list_books = ReadingListBookSerializer(
+        many=True,
+        read_only=True
+    )
+    class Meta:
+        model = ReadingList
+        fields = '__all__'
+
+
+
